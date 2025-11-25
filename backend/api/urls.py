03c72c433c27e463f35c3a62d0e1f069e8272712
Vipin <vn.nawal02@gmail.com>
2025-11-26 00:56:30 +0530

    Made changes to setting.py for deployment

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
-    path("notes/delete//<int:pk>", views.NoteDelete.as_view(), name="delete-note")
+    path("notes/<int:pk>/", views.NoteDelete.as_view(), name="delete-note")
 
 ]
