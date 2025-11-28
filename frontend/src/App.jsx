0e22762f12824a0dc96afcc7e822a9fbe811e278
Vipin <vn.nawal02@gmail.com>
2025-11-28 13:19:29 +0530

    Add navigation bar for the users who are already registered

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Form from "./components/Form";
 import ProtectedRoute from "./components/ProtectedRoute";
+import NavigationBar from "./components/NavigationBar";
 
 // Logout clears tokens and redirects
 function Logout() {
@@ -14,41 +15,44 @@ function Logout() {
 
 function App() {
   return (
+    <>
+      <NavigationBar />
+      <BrowserRouter>
+        <Routes>
 
-    <BrowserRouter>
-      <Routes>
+          {/* HOME (protected) */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* HOME (protected) */}
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
+          {/* LOGIN */}
+          <Route
+            path="/login"
+            element={<Form route="/api/token/" method="login" />}
+          />
 
-        {/* LOGIN */}
-        <Route
-          path="/login"
-          element={<Form route="/api/token/" method="login" />}
-        />
+          {/* REGISTER */}
+          <Route
+            path="/register"
+            element={<Form route="/api/user/register/" method="register" />}
+          />
 
-        {/* REGISTER */}
-        <Route
-          path="/register"
-          element={<Form route="/api/user/register/" method="register" />}
-        />
+          {/* LOGOUT */}
+          <Route path="/logout" element={<Logout />} />
 
-        {/* LOGOUT */}
-        <Route path="/logout" element={<Logout />} />
+          {/* 404 PAGE */}
+          <Route path="*" element={<NotFound />} />
 
-        {/* 404 PAGE */}
-        <Route path="*" element={<NotFound />} />
-
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
+
 }
 
 export default App;
